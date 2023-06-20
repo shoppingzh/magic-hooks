a94b7312046a2868f5526caa173d2db6c6fd0f2a
Shopping <xpzheng6@163.com>
2023-06-20 21:27:46 +0800

    feat: update build script

--- scripts/config.ts
+++ scripts/config.ts
@@ -3,6 +3,10 @@ import fs from 'fs'
 
 const dirs = fs.readdirSync(path.resolve(__dirname, '../src'), { withFileTypes: true })
 
+const devList = [
+  'useLoadList'
+]
+
 export default {
-  entryList: dirs.filter(o => o.isDirectory()).map(o => o.name),
+  entryList: dirs.filter(o => o.isDirectory() && !/^_/.test(o.name) && devList.indexOf(o.name) < 0).map(o => o.name),
 }
