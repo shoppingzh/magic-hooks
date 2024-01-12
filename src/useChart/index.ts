1f6df3521c5b376d8e3cae80ba7430eb7b144989
shoppingzh <xpzheng6@163.com>
2024-01-12 16:54:27 +0800

    feat: useChart, lazy render

--- src/useChart/index.ts
+++ src/useChart/index.ts
@@ -1,20 +1,29 @@
-import { MaybeRef, useResizeObserver, useThrottleFn } from '@vueuse/core'
+import { MaybeRef, useIntersectionObserver, useResizeObserver, useThrottleFn } from '@vueuse/core'
 import * as echarts from 'echarts'
 import { Ref, ShallowRef, onUnmounted, ref, shallowRef, watch } from 'vue'
 import { merge } from 'lodash'
 
+
 export interface UseChartOptions {
   el?: MaybeRef<HTMLElement>
+  /** 加载状态 */
   loading?: MaybeRef<boolean>
+  /** 加载状态风格配置 */
+  loadingOptions?: object // TODO 类型
+  /** ehcarts配置项 */
   option?: Ref<echarts.EChartsOption>
+  /** 主题 */
   theme?: MaybeRef<string>
+  /** 重置大小间隔 */
   resizeDuration?: number
-  loadingOptions?: object
+  /** 懒渲染 !!请在项目中测试功能的可用性，在不支持的浏览器中该功能将失效 */
+  lazyRender?: MaybeRef<boolean>
 }
 
 export interface UseChartReturn {
   el: Ref<HTMLElement>
   loading: Ref<boolean>
+  /** 图表实例 */
   instance: ShallowRef<echarts.EChartsType>
   option: Ref<echarts.EChartsOption>
   theme: Ref<string>
@@ -30,6 +39,18 @@ const DEFAULT_OPTIONS: UseChartOptions = {
   }
 }
 
+function useElementVisible(el: MaybeRef<HTMLElement>) {
+  const visible = ref(false)
+  const { isSupported } = useIntersectionObserver(el, (entries) => {
+    if (!entries || !entries.length) return
+    visible.value = entries[0].isIntersecting
+  })
+  if (!isSupported.value) { // 降级处理：不支持时，默认都显示
+    visible.value = true
+  }
+  return visible
+}
+
 /**
  * 基础图表hooks，支持以下功能：
  * 1. 根据配置自动渲染图表，组件销毁时自动销毁图表实例
@@ -37,6 +58,7 @@ const DEFAULT_OPTIONS: UseChartOptions = {
  * 3. 当容器大小发生变化时，自动重绘图表
  * 4. 当配置项发生变化时，自动重绘图表
  * 5. 动态换肤
+ * 6. 懒渲染
  * @param options 
  * @returns 
  */
@@ -47,9 +69,13 @@ export default function(options: UseChartOptions = {}): UseChartReturn {
   const loading = ref(opts.loading || false)
   const option = ref(opts.option)
   const theme = ref(opts.theme)
+  const lazyRender = ref(opts.lazyRender)
+  const visible = useElementVisible(el) // Vueuse useElementVisibility有问题，自己实现
 
-  function init() {
+  function init(force?: boolean) {
     if (!el.value) return
+    if (lazyRender.value && !visible.value) return
+    if (!force && instance.value) return // 不强制初始化
     instance.value = echarts.init(el.value, theme.value)
   }
 
@@ -87,14 +113,20 @@ export default function(options: UseChartOptions = {}): UseChartReturn {
     }
   }
 
-  watch(el, init, { immediate: true })
-  // FIXME watch el el变为空，也需要销毁
-  onUnmounted(destroy)
-
+  // 元素存在时，渲染
+  watch(el, () => init(), { immediate: true })
+  watch(visible, () => {
+    init()
+  })
+  // 销毁
+  onUnmounted(destroy) // FIXME watch el el变为空，也需要销毁
+  // 元素容器大小变化，重置图表大小
   useResizeObserver(el, useThrottleFn(resize, 500))
-
+  // 配置项变化，重绘
   watch([instance, option], render, { deep: true })
+  // 加载态变化，更新加载
   watch([instance, loading], updateLoading)
+  // 主题变化，重绘
   watch(theme, () => {
     // FIXME 注：echarts暂不支持动态换肤，需要先销毁实例重新初始化
     // 详情请见：https://github.com/apache/echarts/issues/4607
