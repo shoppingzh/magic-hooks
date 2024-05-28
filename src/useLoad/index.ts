fbcca277d3f567ff5296fe15dc3929e08a8de5a8
shoppingzh <xpzheng6@163.com>
2024-05-28 17:20:50 +0800

    feat: useLoad, loaded state

--- src/useLoad/index.ts
+++ src/useLoad/index.ts
@@ -1,20 +1,29 @@
 import { MaybeRef, toReactive } from '@vueuse/core'
-import { Ref, ref } from 'vue'
+import { ComputedRef, Ref, computed, ref } from 'vue'
 
 export type LoadFn<Q, R> = (query: Q) => Promise<R>
 
 export interface UseLoadOptions<Q, R> {
+  /** 初始查询条件 */
   initialQuery?: MaybeRef<Q>
+  /** 默认加载结果 */
   initialResult?: MaybeRef<R>
+  /** 加载中状态 */
   loading?: MaybeRef<boolean>
   /** 自动加载 */
   autoLoad?: boolean
 }
 
 export interface UseLoadReturn<Q, R> {
+  /** 查询条件 */
   query: Q
+  /** 结果 */
   result: Ref<R>
+  /** 加载中状态 */
   loading: Ref<boolean>
+  /** 加载完成状态(只读) */
+  loaded: ComputedRef<boolean>
+  /** 加载 */
   load: (disableLoading?: boolean) => Promise<void>
 }
 
@@ -22,6 +31,8 @@ export interface UseLoadReturn<Q, R> {
  * 通用加载hooks，特性：
  * - 查询参数与加载数据
  * - 不限定加载源
+ * - 加载完成状态
+ * 
  * @param onLoad 
  * @param options 
  * @returns 
@@ -34,6 +45,7 @@ export default function<Q extends object = object, R = unknown>(
   const query = toReactive(opts.initialQuery || {} as Q)
   const result = ref(opts.initialResult) as Ref<R>
   const loading = ref(options.loading)
+  const loaded = ref(false)
 
   function load(disableLoading = false) {
     return new Promise<void>(async(resolve, reject) => {
@@ -43,9 +55,11 @@ export default function<Q extends object = object, R = unknown>(
           loading.value = true
         }
         result.value = await onLoad(query)
+        loaded.value = true
 
         resolve()
       } catch (err) {
+        loaded.value = false
         reject(err)
       } finally {
         loading.value = false
@@ -61,6 +75,7 @@ export default function<Q extends object = object, R = unknown>(
     query,
     result,
     loading,
+    loaded: computed(() => loaded.value),
     load,
   }
 }
